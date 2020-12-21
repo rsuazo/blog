b766aee238c2699e1facabf2021b14928c8fa2f9
rsuazo <rsuazo2@gmail.com>
2020-12-21 15:17:45 -0800

    added welcome controller & index, and styled post#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :users
   resources :posts
+  resources :projects
+  resources :contacts, only: [:new, :create]
+  get 'welcome/index'
+  root 'welcome#index'
+
+  get '*path' => redirect('/')
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
