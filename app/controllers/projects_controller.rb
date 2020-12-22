f088e8252943679b06ccd11950036c58ba3c941b
rsuazo <rsuazo2@gmail.com>
2020-12-21 19:55:59 -0800

    implemented project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,54 @@
+class ProjectsController < ApplicationController
+    before_action :find_project, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+  
+    def index
+      @projects = Project.all.order("created_at desc")
+    end
+  
+    def new
+      @project = Project.new
+    end
+  
+    def create
+      @project = Project.new project_params
+  
+      if @project.save
+        redirect_to @project, notice: "Nice Robert! That project was successfully saved!"
+      else
+        render 'new'
+      end
+    end
+  
+      def show
+  
+      end
+  
+      def edit
+  
+      end
+  
+      def update
+        if @project.update project_params
+          redirect_to @project, notice: "Nice Robert! That project was successfully updated!"
+        else
+          render 'edit'
+        end
+      end
+  
+      def destroy
+        @project.destroy
+        redirect_to projects_path
+      end
+  
+    private
+  
+    def find_project
+      @project = Project.friendly.find(params[:id])
+    end
+  
+    def project_params
+      params.require(:project).permit(:title, :description, :link, :slug)
+    end
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_003922) do
+ActiveRecord::Schema.define(version: 2020_12_22_034946) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2020_12_22_003922) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
