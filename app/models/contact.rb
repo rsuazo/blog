1b80379dd94cd02afc4e182d2ac8e728c7d3a790
rsuazo <rsuazo2@gmail.com>
2020-12-22 11:09:24 -0800

    update sendgrid

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
+	attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
 
   def headers 
     {
-	  :subject => "Contact Form from #{email}",
-	  :to => "rsuazo2@gmail.com",
-	  :from => %("#{name}" <'r69er87@gmail.com'>)
-	}
-  end
+	  	:subject => "Contact Form from: #{name}; #{email}",
+	  	:to => "rsuazo2@gmail.com",
+	  	:from => 'r69er87@gmail.com'
+	  }
+	end
 end
